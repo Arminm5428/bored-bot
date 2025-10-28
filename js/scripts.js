96fa9764b11859b83c26e43f68a21107beb592da
Armin Masoomi <156455307+Arminm5428@users.noreply.github.com>
2025-10-28 16:29:41 +0330

    Update button click to set title and background
    On clicking the bored button, the heading text is now set to 'HappyBot' and the background image is updated with a random gradient. Also cleaned up commented-out code for clarity.

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,8 @@
-/**
-Challenge:
-
-1. Google for "how to use fetch with json"
-2. Should find a top result from MDN
-3. MANUALLY copy over the fetch, but using the URL below 👇
-    https://dog.ceo/api/breeds/image/random
-*/
-// async function getData() {
-//   const url = "https://dog.ceo/api/breeds/image/random";
-//   try {
-//     const response = await fetch(url);
-//     if (!response.ok) {
-//       throw new Error(`Response status: ${response.status}`);
-//     }
-
-//     const result = await response.json();
-//     console.log(result);
-//   } catch (error) {
-//     console.error(error.message);
-//   }
-// }
-
-// getData();
-
-
-
-// fetch("https://dog.ceo/api/breeds/image/random")
-//     .then(res => res.json())
-//     .then(data => document.querySelector("body").innerHTML = `<img src=${data.message} width=400 >`)
-
-
-//////////////////////////////////////////////////////////////////////////
-
-// const heading = document.querySelector(".heading")
-
-// fetch("https://apis.scrimba.com/bored/api/activity")
-//     .then(res => res.json())
-//     .then(data => {
-//         console.log(data["activity"])
-//         heading.innerHTML = `<h1>${data["activity"]}</h1>`
-//         }
-//     )
-        
-
-//////////////////////////////////////////////////////////////////////////
 
 const body = document.querySelector("body");
 const boredBtn = document.querySelector(".bored-btn");
 const idea = document.querySelector(".idea");
+const title = document.querySelector(".heading");
 const bgColors = [
     "radial-gradient( #CD2C58, #FFE6D4)",
     "radial-gradient( #016B61, #9ECFD4)",
@@ -62,7 +17,6 @@ const bgColors = [
 ]
 
 
-
 boredBtn.addEventListener("click", () => {
     fetch("https://apis.scrimba.com/bored/api/activity")
         .then(res => res.json())
@@ -70,7 +24,8 @@ boredBtn.addEventListener("click", () => {
             idea.textContent = data["activity"];
         })
     boredBtn.blur();
-    body.style.backgroundImage = bgColors[Math.floor(Math.random() * 10)]
+    title.textContent = "HappyBot";
+    body.style.backgroundImage = bgColors[Math.floor(Math.random() * 10)];
 })
 
 
